010bb3ebaedd7b1f89522ac73bbef963dbb7644d
Yash Sachdev <140938066+sachdevyash123@users.noreply.github.com>
2024-12-04 17:35:19 +0530

    Add files via upload

--- client/src/utils/TechstackList.js
+++ client/src/utils/TechstackList.js
@@ -1,1 +1,56 @@
+import { FaHtml5,FaCss3,FaJs,FaReact,FaNode,FaBootstrap,FaJava } from "react-icons/fa";
+import { SiExpress,SiMongodb,SiGithub } from "react-icons/si";
+export const TechstackList=[
+    {
+        _id:1,
+        name:'HTML',
+        icon:FaHtml5
+},
+{
+    _id:2,
+    name:'CSS',
+    icon:FaCss3
+},
+{
+    _id:3,
+    name:'Javascript',
+    icon:FaJs
+},
+{
+    _id:4,
+    name:'ReactJs',
+    icon:FaReact
+},
+{
+    _id:5,
+    name:'NodeJS',
+    icon:FaNode
+},
+{
+    _id:6,
+    name:'ExpressJs',
+    icon:SiExpress
+},
+{
+    _id:7,
+    name:'MongoDB',
+    icon:SiMongodb
+},
+{
+    _id:8,
+    name:'Bootstrap',
+    icon:FaBootstrap
+},
+{
+    _id:9,
+    name:'Git / Github',
+    icon:SiGithub
+},
+{
+    _id:10,
+    name:'Core Java',
+    icon:FaJava
+},
 
+
+]
